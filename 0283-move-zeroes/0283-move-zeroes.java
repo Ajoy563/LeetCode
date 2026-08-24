4cb2daa3871534b2cdaefbc9f0ed9dbfa7faef04
Ajoy <dasajoy661@gmail.com>
2026-08-24 22:31:29 +0530

    Time: 2 ms (91.96%), Space: 48 MB (6.9%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,27 @@
 class Solution {
+    // public void moveZeroes(int[] nums) {
+    //     int idx = 0;
+    //     for(int i=0; i<nums.length; i++) {
+    //         if(nums[i] != 0) {
+    //             nums[idx] = nums[i];
+    //             idx++;
+    //         }
+    //     }
+        
+    //     while(idx < nums.length) {
+    //         nums[idx] = 0;
+    //         idx++;
+    //     }
+    // }
     public void moveZeroes(int[] nums) {
         int idx = 0;
         for(int i=0; i<nums.length; i++) {
             if(nums[i] != 0) {
-                nums[idx] = nums[i];
+                int temp = nums[i];
+                nums[i] = 0; 
+                nums[idx] = temp;
                 idx++;
             }
-        }
-        
-        while(idx < nums.length) {
-            nums[idx] = 0;
-            idx++;
         }
     }
 }
