8fb113f55b224eb0504b6cb3df0b0c31bc8057be
Ajoy <dasajoy661@gmail.com>
2026-08-27 23:32:34 +0530

    Time: 0 ms (100%), Space: 47 MB (23.2%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,26 +1,43 @@
 class Solution {
+    // public void duplicateZeros(int[] arr) {
+    //     int n = arr.length - 1, cntZero = 0;
+    //     for (int i = 0; i <= n - cntZero; i++) {
+    //         if (arr[i] == 0) {
+    //             if (i == n - cntZero) {
+    //                 arr[n] = 0;
+    //                 n--;
+    //                 break;
+    //             }
+    //             cntZero++;
+    //         }
+    //     }
+    //     int lastIdx = n - cntZero;
+    //     for (int i = lastIdx; i >= 0; i--) {
+    //         if(arr[i] == 0) {
+    //             arr[i + cntZero] = 0;
+    //             cntZero--;
+    //             arr[i + cntZero] = 0;
+    //         }
+    //         else {
+    //             arr[i + cntZero] = arr[i];
+    //         }
+    //     }
+    // }
+
     public void duplicateZeros(int[] arr) {
-        int n = arr.length - 1, cntZero = 0;
-        for (int i = 0; i <= n - cntZero; i++) {
-            if (arr[i] == 0) {
-                if (i == n - cntZero) {
-                    arr[n] = 0;
-                    n--;
-                    break;
-                }
-                cntZero++;
+        int n = arr.length;
+        int[] res= Arrays.copyOf(arr, n);
+        int i=0;
+        int j=0;
+        while(i<n && j<n){
+            arr[j] = res[i];
+            j++;
+            if (res[i] == 0 && j < n) {
+                arr[j] = 0;
+                j++;
             }
+            i++;
         }
-        int lastIdx = n - cntZero;
-        for (int i = lastIdx; i >= 0; i--) {
-            if(arr[i] == 0) {
-                arr[i + cntZero] = 0;
-                cntZero--;
-                arr[i + cntZero] = 0;
-            }
-            else {
-                arr[i + cntZero] = arr[i];
-            }
-        }
+        return;
     }
 }
