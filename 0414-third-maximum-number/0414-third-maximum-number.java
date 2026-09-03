3b9c25bea1c27637e32a6338c16d3023fdcfcf42
Ajoy <dasajoy661@gmail.com>
2026-09-03 21:15:49 +0530

    Time: 3 ms (66.31%), Space: 44.8 MB (55.31%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -23,57 +23,58 @@ class Solution {
     //     return nums[0];
     // }
 
-    // public int thirdMax(int[] nums) {
-    //     //approach 2 - max 1, 2, 3 - 3ms
-    //     Integer max1 = null,
-    //     max2 = null, max3 = null;
-
-    //     for(int i=0; i<nums.length; i++) {
-    //         if(max1 != null && max1 == nums[i]) {
-    //             continue;
-    //         }
-    //         if(max2 != null && max2 == nums[i]) {
-    //             continue;
-    //         }
-    //         if(max3 != null && max3 == nums[i]) {
-    //             continue;
-    //         }
-
-    //         if(max1 == null || nums[i] > max1) {
-    //             max3 = max2;
-    //             max2 = max1;
-    //             max1 = nums[i];
-    //         }
-    //         else if(max2 == null || nums[i] > max2) {
-    //             max3 = max2;
-    //             max2 = nums[i];
-    //         }
-    //         else if(max3 == null || nums[i] > max3) {
-    //             max3 = nums[i];
-    //         }
-    //     }
-
-    //     if(max3 != null) {
-    //         return max3;
-    //     }
-    //     return max1;
-    // }
-
     public int thirdMax(int[] nums) {
-        Set<Integer> s = new TreeSet<>(Comparator.reverseOrder());
-        for(int n : nums) {
-            s.add(n);
-        }
-        int i = 1;
-        for(int x : s) {
-            if(s.size() < 3) {
-                return x;
+        //approach 2 - max 1, 2, 3 - 3ms
+        Integer max1 = null,
+        max2 = null, max3 = null;
+
+        for(int i=0; i<nums.length; i++) {
+            if(max1 != null && max1 == nums[i]) {
+                continue;
             }
-            if(i == 3) {
-                return x;
+            if(max2 != null && max2 == nums[i]) {
+                continue;
             }
-            i++;
+            if(max3 != null && max3 == nums[i]) {
+                continue;
+            }
+
+            if(max1 == null || nums[i] > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if(max2 == null || nums[i] > max2) {
+                max3 = max2;
+                max2 = nums[i];
+            }
+            else if(max3 == null || nums[i] > max3) {
+                max3 = nums[i];
+            }
         }
-        return -1;
+
+        if(max3 != null) {
+            return max3;
+        }
+        return max1;
     }
+
+    // public int thirdMax(int[] nums) {
+    //     //approach 3 - using set - 8ms
+    //     Set<Integer> s = new TreeSet<>(Comparator.reverseOrder());
+    //     for(int n : nums) {
+    //         s.add(n);
+    //     }
+    //     int i = 1;
+    //     for(int x : s) {
+    //         if(s.size() < 3) {
+    //             return x;
+    //         }
+    //         if(i == 3) {
+    //             return x;
+    //         }
+    //         i++;
+    //     }
+    //     return -1;
+    // }
 }
