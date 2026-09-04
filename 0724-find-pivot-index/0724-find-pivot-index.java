03ee46c3d847ea58455f92c19428860aa2e8727d
Ajoy <dasajoy661@gmail.com>
2026-09-04 21:30:00 +0530

    Time: 0 ms (100%), Space: 47.2 MB (78.35%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -4,18 +4,13 @@ class Solution {
         for(int i=0; i<nums.length; i++) {
             totalSum += nums[i];
         }
-        int rightSum = totalSum;
         for(int i=0; i<nums.length; i++) {
-            if(i - 1 < 0) {
-                rightSum -= nums[i];
-            }
-            else {
-                rightSum -= nums[i];
-                leftSum += nums[i-1];
-            }
+            int rightSum = totalSum - leftSum - nums[i];
+
             if(leftSum == rightSum) {
                 return i;
             }
+            leftSum += nums[i];
         }
         return -1;
     }
