9efb59d3e3399a8963bd69d89db247f892679aef
Ajoy <dasajoy661@gmail.com>
2026-09-04 22:45:57 +0530

    Time: 0 ms (100%), Space: 43.6 MB (10.07%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
@@ -1,18 +1,38 @@
 class Solution {
+    // public int dominantIndex(int[] nums) {
+    //     int max = Integer.MIN_VALUE, idx = 0;
+    //     for(int i=0; i<nums.length; i++) {
+    //         if(nums[i] > max) {
+    //             max = nums[i];
+    //             idx = i;
+    //         }
+    //     }
+
+    //     for(int n : nums) {
+    //         if(n * 2 > max && n != max) {
+    //             return -1;
+    //         }
+    //     }
+    //     return idx;
+    // }
+    
     public int dominantIndex(int[] nums) {
-        int max = Integer.MIN_VALUE, idx = 0;
+        int max1 = Integer.MIN_VALUE, idx = 0, max2 = Integer.MIN_VALUE;
         for(int i=0; i<nums.length; i++) {
-            if(nums[i] > max) {
-                max = nums[i];
+            if(nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
                 idx = i;
+            }
+            else if(nums[i] > max2 && nums[i] != max1) {
+                max2 = nums[i];
             }
         }
 
-        for(int n : nums) {
-            if(n * 2 > max && n != max) {
-                return -1;
-            }
+        if(max2 * 2 > max1) {
+            return -1;
         }
         return idx;
     }
+
 }
