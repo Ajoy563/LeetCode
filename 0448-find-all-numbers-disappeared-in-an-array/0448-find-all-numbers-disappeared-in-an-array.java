6c6a31ec68b3e022b843fb7d2c35626f7fdf1c53
Ajoy <dasajoy661@gmail.com>
2026-09-04 20:57:45 +0530

    Time: 6 ms (84.76%), Space: 67 MB (73.7%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,35 +1,36 @@
 class Solution {
-    // public List<Integer> findDisappearedNumbers(int[] nums) {
-    //     List<Integer> res = new ArrayList<>();
-    //     for(int i=0; i<nums.length; i++) {
-    //         int idx = Math.abs(nums[i]) - 1;
-    //         if(nums[idx] < 0) {
-    //             continue;
-    //         }
-    //         else {
-    //             nums[idx] = - nums[idx];
-    //         }
-    //     }
-    //     for(int i=0; i<nums.length; i++) {
-    //         if(nums[i] > 0) {
-    //             res.add(i+1);
-    //         }
-    //     }
-    //     return res;
-    // }
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        Set<Integer> s = new HashSet<>();
         List<Integer> res = new ArrayList<>();
-
         for(int i=0; i<nums.length; i++) {
-            s.add(nums[i]);
+            int idx = Math.abs(nums[i]) - 1;
+            if(nums[idx] < 0) {
+                continue;
+            }
+            else {
+                nums[idx] = - nums[idx];
+            }
         }
-        for(int i=1; i<=nums.length; i++) {
-            if(!s.contains(i)) {
-                res.add(i);
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] > 0) {
+                res.add(i+1);
             }
         }
         return res;
     }
+
+    // public List<Integer> findDisappearedNumbers(int[] nums) {
+    //     Set<Integer> s = new HashSet<>();
+    //     List<Integer> res = new ArrayList<>();
+
+    //     for(int i=0; i<nums.length; i++) {
+    //         s.add(nums[i]);
+    //     }
+    //     for(int i=1; i<=nums.length; i++) {
+    //         if(!s.contains(i)) {
+    //             res.add(i);
+    //         }
+    //     }
+    //     return res;
+    // }
 
 }
